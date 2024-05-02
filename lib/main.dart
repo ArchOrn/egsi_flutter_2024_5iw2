7726f5c6b14f0a4c46fe4e02319d0288df734466
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-05-02 18:12:36 +0200

    Demo with all classic Navigator methods

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_5iw2/calendar/calendar_screen.dart';
-import 'package:flutter_5iw2/home/home_screen.dart';
+import 'package:flutter_5iw2/b.dart';
+import 'package:flutter_5iw2/c.dart';
+import 'package:flutter_5iw2/home.dart';
+import 'package:flutter_5iw2/route_404.dart';
 
 void main() {
   runApp(const App());
@@ -13,18 +15,21 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: const Home(),
       routes: {
-        '/': (context) => const HomeScreen(),
+        '/b': (context) => const B(),
       },
       onGenerateRoute: (settings) {
         final args = settings.arguments;
         switch (settings.name) {
-          case '/calendar':
+          case '/c':
             return MaterialPageRoute(builder: (context) {
-              return CalendarScreen(username: args as String);
+              return C(text: args as String);
             });
           default:
-            return null;
+            return MaterialPageRoute(builder: (context) {
+              return const Route404();
+            });
         }
       },
       theme: ThemeData(
