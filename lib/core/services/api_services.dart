9a8fccf845da50d351e433fb34e25f895d9894b9
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-05-02 16:36:09 +0200

    Demo with GridView of images retrieved online

--- lib/core/services/api_services.dart
+++ lib/core/services/api_services.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'dart:developer';
 
+import 'package:flutter_5iw2/core/models/photo.dart';
 import 'package:flutter_5iw2/core/models/post.dart';
 import 'package:flutter_5iw2/core/models/user.dart';
 import 'package:http/http.dart' as http;
@@ -42,6 +43,26 @@ class ApiServices {
           }).toList() ?? [];
     } catch (error) {
       log('Error occurred while retrieving users.', error: error);
+      rethrow;
+    }
+  }
+
+  static Future<List<Photo>> getPhotos() async {
+    try {
+      final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+      // Simulate call length for loader display
+      await Future.delayed(const Duration(seconds: 1));
+
+      if (response.statusCode < 200 || response.statusCode >= 400) {
+        throw Error();
+      }
+
+      final data = json.decode(response.body);
+      return (data as List<dynamic>?)?.map((e) {
+        return Photo.fromJson(e);
+      }).toList() ?? [];
+    } catch (error) {
+      log('Error occurred while retrieving photos.', error: error);
       rethrow;
     }
   }
--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_5iw2/core/services/api_services.dart';
 
@@ -10,7 +11,7 @@ class HomeScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.white,
         body: FutureBuilder(
-          future: ApiServices.getUsers(),
+          future: ApiServices.getPhotos(),
           builder: (context, snapshot) {
             final loading = snapshot.connectionState == ConnectionState.waiting;
             if (loading) {
@@ -28,12 +29,29 @@ class HomeScreen extends StatelessWidget {
               );
             }
 
-            return ListView.builder(
+            return GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
               itemBuilder: (context, index) {
-                final user = snapshot.data![index];
-                return ListTile(
-                  title: Text(user.name),
-                  subtitle: Text(user.address),
+                final photo = snapshot.data![index];
+                return Stack(
+                  children: [
+                    Image.network(photo.url),
+                    Positioned(
+                      bottom: 0,
+                      left: 0,
+                      right: 0,
+                      child: Container(
+                        color: Colors.black.withOpacity(.2),
+                        child: Text(
+                          photo.title,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
+                    )
+                  ],
                 );
               },
               itemCount: snapshot.hasData ? snapshot.data!.length : 0,
