d50f8ac2991b0c247f3fe8ff956ff945f7dde470
Raphaël Kiffer <kiffer.raphael@gmail.com>
2024-05-02 15:01:03 +0200

    Demo with basic api http calls and display

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,14 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_5iw2/core/models/post.dart';
+import 'package:flutter_5iw2/core/services/api_services.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Post> _posts = [];
+  bool _loading = false;
+  Error? _error = null;
+
+  @override
+  void initState() {
+    super.initState();
+
+    setState(() {
+      _error = null;
+      _loading = true;
+    });
+
+    ApiServices.getPosts().then((data) {
+      _posts = data;
+    }).catchError((error) {
+      _error = error;
+    }).whenComplete(() {
+      setState(() {
+        _loading = false;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Container(),
+        body: _loading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : (_error != null
+                ? Center(
+                    child: Text(
+                      _error.toString(),
+                      style: const TextStyle(color: Colors.red),
+                    ),
+                  )
+                : ListView.builder(
+                    itemBuilder: (context, index) {
+                      final post = _posts[index];
+                      return ListTile(
+                        title: Text(post.title),
+                        subtitle: Text(post.body),
+                      );
+                    },
+                    itemCount: _posts.length,
+                  )),
       ),
     );
   }
